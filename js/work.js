f02ad309e76c47ad5bf599ed8ede3bf110cbab19
Alex <acroce@udel.edu>
2024-03-03 10:46:39 -0500

    console log

--- js/work.js
+++ js/work.js
@@ -1,27 +1,28 @@
+
 function onButtonClick() {
-    log('Requesting Bluetooth Device...');
+    console.log('Requesting Bluetooth Device...');
     navigator.bluetooth.requestDevice({filters: [{services: ['c5816eb9-773f-4478-aa3b-c466da9ec23d']}]})
     .then(device => {
-      log('Connecting to GATT Server...');
+      console.log('Connecting to GATT Server...');
       return device.gatt.connect();
     })
     .then(server => {
-      log('Getting Heart Rate Service...');
+      console.log('Getting Heart Rate Service...');
       return server.getPrimaryService('c5816eb9-773f-4478-aa3b-c466da9ec23d');
     })
     .then(service => {
-      log('Getting Heart Rate Control Point Characteristic...');
+      console.log('Getting Heart Rate Control Point Characteristic...');
       return service.getCharacteristic('a290ac53-b59f-4254-b910-c5044e1942c8');
     })
     .then(characteristic => {
-      log('Writing Heart Rate Control Point Characteristic...');
+      console.log('Writing Heart Rate Control Point Characteristic...');
   
       // Writing 1 is the signal to reset energy expended.
       let resetEnergyExpended = Uint8Array.of(1);
       return characteristic.writeValue(resetEnergyExpended);
     })
-    .then(device => {log('> Energy expended has been reset.');})
+    .then(() => {log('> Energy expended has been reset.');})
     .catch(error => {
-      log('Argh! ' + error);
+      console.log('Argh! ' + error);
     });
   }
